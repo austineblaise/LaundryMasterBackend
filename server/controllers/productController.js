9f8ef258831d099ed97d2acaec6bd5731704a67d
austine-blaise <austineblaise@gmail.com>
2023-06-15 22:40:46 +0100

    changed something

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,7 +1,7 @@
  const getProducts = async (req, res, next) => {
 	res.status(200).json({
 		success: true,
-		message: "this route will show all route in the data base",
+		message: "this route will show all route in the data base, yea",
 	});
 };
 
